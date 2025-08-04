d370920d32b8bbb3869abbb8a93ac8535412ff8c
Chris Varughese <cjamiev@gmail.com>
2025-08-03 20:11:43 -0500

    feat: updating ports

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 4510,
     proxy: {
       '/storage': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:4010',
         changeOrigin: true,
       }
     }
